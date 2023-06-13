d4cb136eb4c19231a8a877ab262082abc28ab7b3
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-13 01:49:02 -0700

    shunchaki ishlandiqu

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,14 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        int sum = 0;
-        int num = 9;
-
+        int sum1= 0;
+        int sum2 = 9;
         for (int i = 1; i <= lengthOfLastNumber; i++) {
-            sum += num;
-            num = num * 10 + 9;
+            sum1 += sum2;
+            sum2 = sum2 * 10 + 9;
         }
 
-        System.out.println(sum);
+        System.out.println(sum1);
     }
     }
