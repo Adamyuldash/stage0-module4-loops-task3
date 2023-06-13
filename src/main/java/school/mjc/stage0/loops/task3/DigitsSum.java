5ae19158c1f5e0a2621a93c3ce26f7d00c94cbf7
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-13 01:29:44 -0700

    shunchaki ishlandi

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,13 +1,11 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t) {
-    int sum = 0;
-        for (int i = 0; i <=t ; i++) {
-            sum = t %10;
-            sum=t/10;
-
-        }
-        System.out.println(sum);
+    public void printDigitsSum(int number) {
+    int sum=0;
+    for (int i = Math.abs(number); i > 0 ; i=i/10) {
+        sum += i %10;
+    }
+    System.out.println(sum);
     }
 }
